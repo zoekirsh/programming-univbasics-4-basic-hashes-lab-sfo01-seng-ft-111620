026ab1b266cdd22b22d59644e32adb99534780a1
maxwellbenton <maxwellbenton@gmail.com>
2020-02-29 18:59:08 -0500

    update starter code

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -9,3 +9,8 @@ end
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
+
+
+def id_hash_generator(number)
+  # return a hash with a key :id assigned to the provided number
+end
