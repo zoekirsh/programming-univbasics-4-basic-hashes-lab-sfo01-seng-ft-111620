cb9de51000a8f9e981713e4a5b83369e9d82d741
maxwellbenton <maxwellbenton@gmail.com>
2020-02-29 15:34:51 -0500

    move solution file, remove later methods

---
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -0,0 +1,11 @@
+def new_hash
+  # return an empty hash
+end
+
+def my_hash
+  # return a valid hash with any key/value pair of your choice
+end
+
+def pioneer
+  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+end
