adea9f50d8e19c8861709c522ca4135ecdd08118
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-12 23:54:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def new_hash
-  # return an empty hash
+  empty_hash = {} 
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  plants = {:monstera => "swiss cheese plant", :calathea => "peacock plant", :sansevierina} 
 end
 
 def pioneer
